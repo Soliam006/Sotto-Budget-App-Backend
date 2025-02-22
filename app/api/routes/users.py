81ac3da5d3e4a767908237e079c0f312ce19a92f
Soliam006 <wsottoja7@alumnes.ub.edu>
2025-02-22 16:26:36 +0100

    Response updated

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -16,34 +16,40 @@ def get_first_user(session: Session = Depends(get_session)):
 
 # Endpoint para obtener todos los usuarios
 @router.get("/all",
-            response_model=UsersOut)
+            response_model=Response)
 def get_all_users(session: Session = Depends(get_session)):
     users = crud.get_all_users(session=session)
 
-    users_out = [
-        UserOut(
-            id=user.id,
-            name=user.name,
-            email=user.email,
-            role=user.role,
-            language_preference=user.language_preference
-        ) for user in users
-    ]
+    if not users:
+        return Response(statusCode=404, data=None, message="Users not found")
 
-    return UsersOut(users=users_out)
+    try:
+        users_out = [
+            UserOut(
+                id=user.id,
+                name=user.name,
+                email=user.email,
+                role=user.role,
+                language_preference=user.language_preference
+            ) for user in users
+        ]
+    except Exception as e:
+        return Response(statusCode=400, data=None, message="Error parsing users")
+
+    return Response(statusCode=200, data=UsersOut(users=users_out), message="Users found")
 
 
 @router.get("/{user_id}")
-async def read_user(user_id: int, session: Session = Depends(get_session)):
+async def read_user(user_id: int, session: Session = Depends(get_session))->Response:
     user = crud.get_user(session=session, user_id=user_id)
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+        return Response(statusCode=404, data=None, message="User not found")
+    return Response(statusCode=200, data=user, message="User found")
 
 
 @router.post("/")
-async def create_user(new_user: UserCreate, session: Session = Depends(get_session)):
+async def create_user(new_user: UserCreate, session: Session = Depends(get_session))->Response:
     user = crud.create_user(session=session, user_create=new_user)
     if user is None:
-        raise HTTPException(status_code=400, detail="User not created")
-    return user
+        return Response(statusCode=400, data=None, message="Error creating user")
+    return Response(statusCode=200, data=user, message="User created")
